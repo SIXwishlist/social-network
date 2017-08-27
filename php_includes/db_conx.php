7dbad6665ac905e406fb8c7ff22a18035db5db61
Tanmay Rauth <tanmayrauth123@gmail.com>
2017-08-27 12:53:14 +0530

    Update db_conx.php

--- php_includes/db_conx.php
+++ php_includes/db_conx.php
@@ -1,5 +1,5 @@
 <?php
-$connection=mysqli_connect("localhost","root","","social_network");
+$connection=mysqli_connect("mysql://mysql:3306/","root","T24091996","sampledb");
 // check for connection
 if (mysqli_connect_errno())
     {
