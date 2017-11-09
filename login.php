cf9a965204d30592f6ee1b8f8ae4413357a2d4fb
Tanmay Rauth <tanmayrauth123@gmail.com>
2017-11-09 17:51:05 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 include_once("php_includes/check_login_status.php");
-// If user is already logged in, header that weenis away
+// If user is already logged in, header that weenis away to his/her profile page
 if($user_ok == true){
 	header("location: user.php?u=".$_SESSION["username"]);
     exit();
